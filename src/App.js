e1c2962eb6944cc962027c7de3e10b21dc5e7b76
mohamed <mohamedksila4@gmail.com>
2023-01-01 13:56:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './CSS/form.css'
+
+
+function App() {
+return(
+  <div>
+<div className="collapse" id="navbarToggleExternalContent">
+  <div className="bg-dark p-4">
+    <h5 className="text-white h4">Collapsed content</h5>
+    <span className="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
+<nav className="navbar navbar-dark bg-dark">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+
+<form className='form'>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn-primary">Submit</button>
+</form>
+</div>
+
+
+
+)
+}
+
+export default App;
